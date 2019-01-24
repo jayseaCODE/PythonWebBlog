44bb206d0ef4e4d96bd94a089f6cf572d341962e
jayseaCODE <jayseaLEARN@hotmail.com>
2019-01-24 13:02:54 +1300

    Hook up the desired post_detail path url to a TO-DO view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url('', views.post_list, name='post_list'),
+    # Django expects an integer value and will transfer it to the view as variable 'pk'
+    url('post/<int:pk>/', views.post_detail, name='post_detail' ),
 ]
