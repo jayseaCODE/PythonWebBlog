f2f06556a69d60166de24e21a247e7b37260c5b9
jayseaCODE <jayseaLEARN@hotmail.com>
2019-01-25 00:11:33 +1300

    fix: regular expression in post detail's url matching pattern
    Named groups is used in the Python regex for the variable "pk"

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url('', views.post_list, name='post_list'),
     # Django expects an integer value and will transfer it to the view as variable 'pk'
-    url('post/<int:pk>/', views.post_detail, name='post_detail' ),
+    url(r'^post/(?P<pk>\d{1,2})/$', views.post_detail, name='post_detail' ),
 ]
